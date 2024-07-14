9900fb0bedd58b678cc209d1c726d8c60f7b7aa6
waliulohan <waliulislam1742@gmail.com>
2024-07-14 20:28:43 +0600

    addded menu modal with animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 "use client"
+import { motion } from "framer-motion"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
@@ -14,8 +15,49 @@ const menuLinks = [
 const Navbar = ({ }) => {
     const [isOpen, setIsOpen] = useState(false)
     const pathname = usePathname()
+     
+    const topVariants = {
+        closed: {
+          rotate: 0,
+        },
+        opened: {
+          rotate: 45,
+          
+        },
+      };
+      const centerVariants = {
+        closed: {
+          opacity: 1,
+        },
+        opened: {
+          opacity: 0,
+        },
+      };
+    
+      const bottomVariants = {
+        closed: {
+          rotate: 0,
+        },
+        opened: {
+          rotate: -45,
+          
+        },
+      };
+    
+      const listVariants = {
+        closed: {
+          x: "100vw",
+        },
+        opened: {
+          x: 0,
+          transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.2,
+          },
+        },
+      };
 
-    return <div className="h-full flex justify-between items-center px-4 md:px-12 lg:px-20  xl:px-48">
+    return <div className="h-full flex justify-between items-center px-4 md:px-12 lg:px-20  xl:px-48 ">
         {/* LINKS  */}
         <div className=" text-black hidden md:flex  items-center justify-center text-lg font-semibold gap-3 w-2/5">
                 {menuLinks.map(link => {
@@ -42,12 +84,38 @@ const Navbar = ({ }) => {
         </div>
 
         {/* MENU ICON */}
-        <div className="md:hidden">
-            <Image src='/menu.png' alt="menu" height={28} width={28} className="z-50 relative" onClick={() => setIsOpen(!isOpen)} />
-            {isOpen && <div className="absolute top-0 left-0 w-screen h-screen bg-green-300 text-white flex flex-col items-center justify-center gap-8 text-4xl ">
+        <div className="md:hidden" onClick={()=> setIsOpen(!isOpen)}>
+            <motion.div className="w-10 h-8 flex flex-col justify-between z-50 relative">
+                <motion.div 
+                    variants={topVariants}
+                    animate={isOpen ? "opened" : "closed"}
+                    className="h-1 w-10 bg-black rounded-full  origin-left" 
+                 />
+                <motion.div 
+                    variants={centerVariants}
+                    animate={isOpen ? "opened" : "closed"}
+                    className="h-1 w-10 bg-black rounded-full  " 
+                 />
+                <motion.div 
+                    variants={bottomVariants}
+                    animate={isOpen ? "opened" : "closed"}                   
+                    className="h-1 w-10 bg-black rounded-full  origin-left" 
+                />
+            </motion.div>
+            {isOpen && <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 h-3/4 bg-green-50  text-black font-semibold flex flex-col items-center justify-center gap-5 text-4xl z-20
+            bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-xl border border-white border-opacity-20 shadow-lg
+            ">
                 {menuLinks.map(link => {
                     return <Link key={link.link} href={link.link} >{link.title}</Link>
                 })}
+                <div className="flex justify-center items-center gap-5 mt-2">
+                    <Link href="#">
+                    <Image src='/github.png' alt="menu" height={28} width={28}/>
+                    </Link>
+                    <Link href="#">
+                    <Image src='/twitter.png' alt="menu" height={28} width={28}/>
+                    </Link>
+                </div>
             </div>}
         </div>
     </div>
