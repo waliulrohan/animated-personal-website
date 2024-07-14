08a76b49d7824f150df785004491cbb2b23963aa
waliulohan <waliulislam1742@gmail.com>
2024-07-14 19:31:05 +0600

    added hero page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div className="">
-hiiii
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 overflow-auto md:overflow-hidden">
+      {/* IMAGE CONTAINER */}
+      <div className="h-2/5 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero.png" alt="" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-3/5 lg:h-full lg:w-1/2 flex flex-col gap-4 md:gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-2xl md:text-6xl font-bold">
+          Crafting Digital Experiences, Designing Tomorrow.
+        </h1>
+        {/* DESC */}
+        <p className="md:text-xl">
+          Welcome to my digital canvas, where innovation and creativity
+          converge. With a keen eye for aesthetics and a mastery of code.
+        </p>
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+        <button className="p-3 rounded-lg ring-1 ring-black bg-black text-white">
+          View My Work
+        </button>
+        <button className="p-2 rounded-lg ring-1 ring-black">
+          Contact Me
+        </button>
+      </div>
+      </div>
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = ({ }) => {
         {/* LINKS  */}
         <div className=" text-black hidden md:flex  items-center justify-center text-lg font-semibold gap-3 w-2/5">
                 {menuLinks.map(link => {
-                    return <Link className={`rounded p-1 ${pathname === link.link && 'bg-black text-white'}`} key={link.link} href={link.link} >{link.title}</Link>
+                    return <Link className={`hover:bg-black hover:text-white rounded p-1 ${pathname === link.link && 'bg-black text-white '}`} key={link.link} href={link.link} >{link.title}</Link>
                 })}
         </div>
 
@@ -27,7 +27,7 @@ const Navbar = ({ }) => {
         <div className="md:hidden w-1/5 lg:flex justify-center items-center">
             <Link href='/' className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-between w-fit ">
                 <span className="text-white mr-1 flex items-center justify-center  ">Waliul</span>
-                <span className="text-black rounded-md bg-white h-8 w-12 flex items-center justify-center " >Rohan</span>
+                <span className="text-black rounded-md bg-white h-8 w-12 flex items-center justify-center " >ruhan</span>
             </Link>
         </div>
 
